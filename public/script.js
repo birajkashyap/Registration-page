0ae226b68c82607d4a22056e6a1a0ec6e1654d9e
Biraj Kashyap <kashyapbiraj83gmail.com>
2024-06-01 23:12:43 +0530

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,20 @@
+// public/script.js
+document.getElementById('registrationForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    const response = await fetch('/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+  
+    const result = await response.text();
+    document.getElementById('response').innerText = result;
+  });
+  
